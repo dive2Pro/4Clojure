53435d09e2abc224e35435bff7d3d61be6c785b8
dive2Pro <toylucklebo@gmail.com>
2020-06-23 16:23:25 +0800

    #33

--- src/clo_4clojure/core.clj
+++ src/clo_4clojure/core.clj
@@ -20,9 +20,9 @@
 (defn pack_seq_dup [x]
   (reduce (fn [p q]
             (if
-              (do (println p q )
-                   (= (last (last p)) q) ;如果最后一个集合的最后一个数字和当前的 q 相同
-                   )
+              (do (println p q)
+                  (= (last (last p)) q)                     ;如果最后一个集合的最后一个数字和当前的 q 相同
+                  )
               (conj (vec (drop-last p)) (conj (first (take-last 1 p)) q)) ; 放入相同集合
               (conj p [q])                                  ; 使用 q 构造一个新的集合
               )
@@ -33,4 +33,20 @@
 
 (defn pack_repeat [x]
   (reduce #(conj %1 %2 %2) [] x)
+  )
+
+(defn pack_repeat_x [arr times]
+  (let [repeat (fn [val count]
+                 (map (fn [_v] val) (range count))
+                 )]
+    (reduce #(concat %1 (repeat %2 times)) [] arr)
+    )
+  )
+; find a way that works like js.bind
+(defn pack_repeat_x2 [arr times]
+  (let [repeat (fn [val count]
+                 (map (fn [_v] val) (range count))
+                 )]
+      (mapcat #(repeat %1 times) arr)
+    )
   )
